3d5a7fe1816a34d4230bd478e234f4232c73fdf5
fmauricios <mauricioserf@hotmail.com>
2015-07-27 21:53:36 -0500

    Agrgue libreria alertify

--- js/Validaciones.js
+++ js/Validaciones.js
@@ -1,33 +1,26 @@
-function Validar() {
+$(document).ready(function(){
+	$('#BotonSend').on('click', function () {
+		var nombre = $('.Formulario-name').val();
 
-	var User = document.F1.User.value;
-	var Pass = document.F1.Pass.value;
-	// var Cel=document.Form1.TxtCelular.value;
-	 var Div = document.getElementById("Resultado");
-	// var Ec=document.Form1.TxtEstado.value;
+		alertify.alert(nombre)
 
-	if(User === '' || Pass === '') {
-		alert("faltan datos");
+	})
+});
 
-  } else {
-		document.F1.submit();
-	}
-}
-
-function ValidarFormReg(){
-
-	var Nom      = document.FormReg.nombre.value;
-	var Apellido = document.FormReg.apellido.value;
-	var Fnac   	 = document.FormReg.fnac.value;
-	var EMail		 = document.FormReg.email.value;
-	var Pass		 = document.FormReg.pass.value;
-
-	if(Nom === "" || Apellido === "" || Fnac === "" || EMail === "" || Pass === "") {
-		$(document).ready(function(){
-			alert("faltan campos");
-		});
-
-	} else {
-		document.FormReg.submit();
-	}
-}
+// function ValidarFormReg(){
+//
+// 	var Nom      = document.FormReg.nombre.value;
+// 	var Apellido = document.FormReg.apellido.value;
+// 	var Fnac   	 = document.FormReg.fnac.value;
+// 	var EMail		 = document.FormReg.email.value;
+// 	var Pass		 = document.FormReg.pass.value;
+//
+// 	if(Nom === "" || Apellido === "" || Fnac === "" || EMail === "" || Pass === "") {
+// 		$(document).ready(function(){
+// 			alert("faltan campos");
+// 		});
+//
+// 	} else {
+// 		document.FormReg.submit();
+// 	}
+// }
