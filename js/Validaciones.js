d6228c4a4c72fda7d6c714ce1cfc7151a805e75d
miguel258 <mra258@hotmail.com>
2015-07-29 17:55:30 -0500

    Se incorporo el formulario de loggin y se valido,aspesctos a mejorar el diseño

--- js/Validaciones.js
+++ js/Validaciones.js
@@ -1,10 +1,36 @@
 $(document).ready(function(){
 	$('#BotonSend').on('click', function () {
 		var nombre = $('.Formulario-name').val();
+		var apellido=$('.Formulario-apellido').val();
+		var Fnac=$('.Formulario-fnac').val();
+		var Email=$('.Formulario-email').val();
+		var Pass=$('.Formulario-Contraseña').val();
 
-		alertify.alert(nombre)
+		if(nombre === "" || apellido === "" || Fnac === "" || Email === "" || Pass === "") {
 
+		   alertify.alert("Recuerde llenar todos los campos");
+
+		}
 	})
+});
+$(document).ready(function(){
+	$('#BotonLoggin').on('click',function(){
+			var User=$('.FormularioLog-User').val();
+			var PassLogg=$('.FormularioLog-COntrasela').val();
+			var r=document.getElementById("Barra");
+			var Barra="<div class="+"progress"+"><div class="+"indeterminate #64b5f6"+"></div></div>";
+
+			if(User==='' || PassLogg===''){
+				alertify.alert("Faltan campos por llenar");
+				
+			}else{
+				r.innerHTML=Barra;
+			}
+
+
+
+	})	
+
 });
 
 // function ValidarFormReg(){
